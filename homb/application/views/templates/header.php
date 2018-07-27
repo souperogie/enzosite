67e419893b2311adbd819fa4eb01846f06cf35ce
souperogie <souperogie@gmail.com>
2018-07-28 00:41:30 +0800

    Minor changes

--- homb/application/views/templates/header.php
+++ homb/application/views/templates/header.php
@@ -5,3 +5,13 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/materialize.css">
 </head>
 <body>
+	<nav>
+		<div class="nav-wrapper">
+			<ul id="nav-mobile" class="center hide-on-med-and-down">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Branch</a></li>
+				<li><a href="#">Gallery</a></li>
+			</ul>
+		</div>
+	</nav>
